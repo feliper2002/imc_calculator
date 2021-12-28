3e103cc4553ff1edff0650cb9469214b7188b720
Felipe Ribeiro <feliper.dev@gmail.com>
2021-12-28 15:55:23 -0300

    Usecase fix

--- lib/home/controller/imc_controller.dart
+++ lib/home/controller/imc_controller.dart
@@ -7,15 +7,36 @@ class IMCController {
 
   double? mass;
   double? height;
+  double result = 0;
 
   setMass(double value) {
     mass = value;
+  }
+
+  setResult(double value) {
+    result = value;
   }
 
   setHeight(double value) {
     height = value;
   }
 
-  String imcResult(double height, double mass) =>
-      _calculateIMCUsecase.calculate(height, mass);
+  void calculateIMCResult(double height, double mass) {
+    double calculus = _calculateIMCUsecase.calculate(height, mass);
+    setResult(calculus);
+  }
+
+  String get imcCategory {
+    if (result < 18.5) {
+      return "Magreza - Grau: 0";
+    } else if (result >= 18.5 && result <= 24.9) {
+      return "Normal - Grau: 0";
+    } else if (result >= 25 && result < 29.9) {
+      return "Sobrepeso - Grau: I";
+    } else if (result >= 30 && result < 39.9) {
+      return "Obesidade - Grau: II";
+    } else {
+      return "Obesidade grave - Grau: III";
+    }
+  }
 }
--- lib/home/model/calculate_imc.dart
+++ lib/home/model/calculate_imc.dart
@@ -1,10 +1,10 @@
 abstract class CalculateIMC {
-  String calculate(double height, double mass);
+  double calculate(double height, double mass);
 }
 
 class CalculateIMCImpl implements CalculateIMC {
   @override
-  String calculate(double height, double mass) {
+  double calculate(double height, double mass) {
     Map<String, dynamic> errors = {};
 
     if (height <= 0 || mass <= 0) {
@@ -13,16 +13,6 @@ class CalculateIMCImpl implements CalculateIMC {
 
     double calculus = (mass / (height * height));
 
-    if (calculus < 18.5) {
-      return "Magreza - Grau: 0";
-    } else if (calculus >= 18.5 && calculus <= 24.9) {
-      return "Normal - Grau: 0";
-    } else if (calculus >= 25 && calculus < 29.9) {
-      return "Sobrepeso - Grau: I";
-    } else if (calculus >= 30 && calculus < 39.9) {
-      return "Obesidade - Grau: II";
-    } else {
-      return "Obesidade grave - Grau: III";
-    }
+    return calculus;
   }
 }
